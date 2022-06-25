be9ddd79033e2bcd7473f6d43478f14452464cc9
Jorge <jorgechp@uoc.edu>
2022-06-25 11:24:50 +0200

    Externalize config

---
+++ src/app/config/config.dev.ts
@@ -0,0 +1,4 @@
+export const CONFIGS = {
+  host: "http://127.0.0.1",
+  port: "8000"
+};
--- src/app/services/stats.service.ts
+++ src/app/services/stats.service.ts
@@ -1,29 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CONFIGS } from '../config/config.dev';
+
 
 import { GraphResponse } from '../models/graph_response.model';
 import { Histogram } from '../models/histogram.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class StatsService {
+  private host = CONFIGS.host + ':' + CONFIGS.port;
 
   constructor(private http: HttpClient) { }
 
   getEndorses() {
-    return this.http.get<GraphResponse>('http://127.0.0.1:8000/stats/endorsements');
+
+    return this.http.get<GraphResponse>(this.host + '/stats/endorsements');
   }
 
   getComments() {
-    return this.http.get<GraphResponse>('http://127.0.0.1:8000/stats/comments');
+    return this.http.get<GraphResponse>(this.host + '/stats/comments');
   }
 
   getEndorsesHistogram() {
-    return this.http.get<Histogram>('http://127.0.0.1:8000/stats/endorsements/histogram');
+    return this.http.get<Histogram>(this.host + '/stats/endorsements/histogram');
   }
 
   getCommentsHistogram() {
-    return this.http.get<Histogram>('http://127.0.0.1:8000/stats/comments/histogram');
+    return this.http.get<Histogram>(this.host + '/stats/comments/histogram');
   }
 }
